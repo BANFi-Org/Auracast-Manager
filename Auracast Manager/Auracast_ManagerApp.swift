c1499714d9fde1515dc8e49c0819c74bd2085aa9
Jason <cw@banfiww.com>
2024-04-08 16:34:09 +0800

    Initial Commit

---
+++ Auracast Manager/Auracast_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auracast_ManagerApp.swift
+//  Auracast Manager
+//
+//  Created by Jason on 2024/4/8.
+//
+
+import SwiftUI
+
+@main
+struct Auracast_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Auracast Manager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Auracast Manager
+//
+//  Created by Jason on 2024/4/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
