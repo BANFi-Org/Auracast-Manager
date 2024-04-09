d7cd69658067d0a6663fc54d1f497979332615bd
Jason <cw@banfiww.com>
2024-04-09 19:58:44 +0800

    Splash screen, app icon, and logo

--- Auracast Manager/Auracast_ManagerApp.swift
+++ Auracast Manager/Auracast_ManagerApp.swift
@@ -11,7 +11,44 @@ import SwiftUI
 struct Auracast_ManagerApp: App {
     var body: some Scene {
         WindowGroup {
+            ViewCoordinator()
+        }
+    }
+}
+
+struct ViewCoordinator: View {
+    @State private var isActive = false
+    var body: some View {
+        if isActive {
             ContentView()
+        }else {
+            SplashScreen(isActive: $isActive)
+        }
+    }
+}
+
+struct SplashScreen: View {
+    @State private var scale = 0.7
+    @Binding var isActive: Bool
+    var body: some View {
+        VStack {
+            VStack {
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 220)
+            }.scaleEffect(scale)
+            .onAppear{
+//                withAnimation(.easeIn(duration: 0.7)) {
+//                    self.scale = 0.9
+//                }
+            }
+        }.onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
         }
     }
 }
